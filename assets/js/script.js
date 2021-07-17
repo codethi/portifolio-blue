8e6b9ddb94a6d5002a9ff57b06758521437e9646
limathiagos <docencia.thiago@gmail.com>
2021-07-16 22:48:25 -0300

    Campos validados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 /* Cria a variável inputNome e coloca nela o elemento que possui o id nome */
 let inputNome = document.querySelector('#nome')
+let inputEmail = document.querySelector('#email')
+let textareaMensagem = document.querySelector('#mensagem')
+let btnEnviar = document.querySelector('#enviar')
 
 /* Só posso utilziar a arrow function (=>) quando a função não tiver nome */
 inputNome.addEventListener('keyup', () => { 
@@ -9,4 +12,26 @@ inputNome.addEventListener('keyup', () => {
       inputNome.style.borderColor = 'green'
    }
 })
+
+inputEmail.addEventListener('keyup', () => {
+   if(inputEmail.value.indexOf('@') == -1 || inputEmail.value.indexOf('.') == -1){
+      inputEmail.style.borderColor = 'red'
+   } else {
+      inputEmail.style.borderColor = 'green'
+   }  
+})
+
+textareaMensagem.addEventListener('keyup', ()=>{
+   if(textareaMensagem.value.length > 100){
+      textareaMensagem.style.borderColor = 'red'
+   } else {
+      textareaMensagem.style.borderColor = 'green'
+   }
+})
+
+
+btnEnviar.addEventListener('click', () => {
+   alert('Formulário enviado com sucesso!')
+})
+
 
